004a7fe3c49bca7f951a7900ddfd00f935e38431
mou-svg <mousumi.sr@gmail.com>
2026-02-25 10:58:31 -0500

    security headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -165,6 +165,11 @@ class TestAccountService(TestCase):
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
+    def test_cors_security(self):
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+        
     def test_security_headers(self):
 	    """It should return security headers"""
 	    response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
