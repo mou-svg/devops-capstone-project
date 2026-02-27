90dda16ad379783ee22c367e2d681b864d76697a
mou-svg <mousumi.sr@gmail.com>
2026-02-26 23:09:05 -0500

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -175,11 +175,12 @@ class TestAccountService(TestCase):
 	    response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
 	    self.assertEqual(response.status_code, status.HTTP_200_OK)
 	    headers = {
-		    'X-Frame-Options': 'SAMEORIGIN',
-		    'X-Content-Type-Options': 'nosniff',
-		    'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
-		    'Referrer-Policy': 'strict-origin-when-cross-origin'
+            'X-Frame-Options': 'SAMEORIGIN',
+            'X-Content-Type-Options': 'nosniff',
+            'Content-Security-Policy': "default-src 'self'; object-src 'none'",
+            'Referrer-Policy': 'strict-origin-when-cross-origin'
         }
+
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
 
